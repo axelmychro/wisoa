b6414380d3e944c3e2c9e15f8971f01eb3ef5240
Axel <maxelramadhan@gmail.com>
2025-10-14 22:35:29 +0700

    console.log();

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ for (const folder of commandFolders) {
       client.commands.set(command.data.name, command);
     } else {
       console.log(
-        `[W] the command at ${filePath} is missing a required "data" or "execute" property`
+        `the command at ${filePath} is missing a required "data" or "execute" property`
       );
     }
   }
